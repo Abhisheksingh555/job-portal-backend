0f9284993d990aa2f0223da82845021b7346d460
Abhishek Singh <abhisheksinghdkm143@gmail.com>
2025-11-04 16:36:58 +0530

    final

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
+
 const app = express();
 
-//middleware
+connectDB();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,17 +26,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 5001;
-
- 
-//api's
-
+// API routes
 app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
 
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server is running on port ${PORT}`);
-// });
+export default app;
