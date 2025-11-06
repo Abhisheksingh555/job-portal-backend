20d56b9d06b82db2052660d2e06354e6447d4832
Abhishek Singh <abhisheksinghdkm143@gmail.com>
2025-11-06 14:42:54 +0530

    add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
@@ -12,6 +13,7 @@ dotenv.config();
 
 const app = express();
 
+// Database connection
 connectDB();
 
 // Middleware
@@ -20,10 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://job-portal-frontend-two-nu.vercel.app"],
   credentials: true,
 };
-
 app.use(cors(corsOptions));
 
 // API routes
@@ -32,4 +33,10 @@ app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
 
+// Root test route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+// ✅ Required for Vercel
 export default app;
